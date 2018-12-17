9066cf39aa64030f1078406350c941892c7c0729
Vincent Lepage <Vincent.lepage@owkin.com>
2018-12-17 13:18:06 +0100

    initial commit with deploy and main terraform for AWS Lambda

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+# Get Virtualenv Directory Path
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ -z "$VIRTUAL_ENV_DIR" ]; then
+    VIRTUAL_ENV_DIR="$SCRIPT_DIR/venv"
+fi
+
+echo "Using virtualenv located in : $VIRTUAL_ENV_DIR"
+
+# If zip artefact already exists, back it up
+if [ -f $SCRIPT_DIR/check_file_lambda.zip ]; then
+    mv $SCRIPT_DIR/check_file_lambda.zip $SCRIPT_DIR/check_file_lambda.zip.backup
+fi
+
+# Add virtualenv libs in new zip file
+cd $VIRTUAL_ENV_DIR/lib/python3.6/site-packages
+zip -r9 $SCRIPT_DIR/check_file_lambda.zip *
+cd $SCRIPT_DIR
+
+# Add python code in zip file
+zip -r9 $SCRIPT_DIR/check_file_lambda.zip check_file_lambda.py
+
+# Run terraform apply
+terraform apply
---
+++ main.tf
@@ -0,0 +1,93 @@
+provider "aws" {
+    region = "${var.aws_region}"
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+}
+
+# Cloudwatch event rule
+resource "aws_cloudwatch_event_rule" "check-file-event" {
+    name = "check-file-event"
+    description = "check-file-event"
+    schedule_expression = "cron(0 1 ? * * *)"
+}
+
+# Cloudwatch event target
+resource "aws_cloudwatch_event_target" "check-file-event-lambda-target" {
+    target_id = "check-file-event-lambda-target"
+    rule = "${aws_cloudwatch_event_rule.check-file-event.name}"
+    arn = "${aws_lambda_function.check_file_lambda.arn}"
+    input = <<EOF
+{
+  "bucket": "my_bucket",
+  "file_path": "the_path"
+}
+EOF
+}
+
+# IAM Role for Lambda function
+resource "aws_iam_role" "check_file_lambda" {
+    name = "check_file_lambda"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+data "aws_iam_policy_document" "s3-access-ro" {
+    statement {
+        actions = [
+            "s3:GetObject",
+            "s3:ListBucket",
+        ]
+        resources = [
+            "arn:aws:s3:::*",
+        ]
+    }
+}
+
+resource "aws_iam_policy" "s3-access-ro" {
+    name = "s3-access-ro"
+    path = "/"
+    policy = "${data.aws_iam_policy_document.s3-access-ro.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "s3-access-ro" {
+    role       = "${aws_iam_role.check_file_lambda.name}"
+    policy_arn = "${aws_iam_policy.s3-access-ro.arn}"
+}
+
+resource "aws_iam_role_policy_attachment" "basic-exec-role" {
+    role       = "${aws_iam_role.check_file_lambda.name}"
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# AWS Lambda function
+resource "aws_lambda_function" "check_file_lambda" {
+    filename = "check_file_lambda.zip"
+    function_name = "check_file_lambda"
+    role = "${aws_iam_role.check_file_lambda.arn}"
+    handler = "check_file_lambda.handler"
+    runtime = "python2.7"
+    timeout = 10
+    source_code_hash = "${base64sha256(file("check_file_lambda.zip"))}"
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_file" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = "${aws_lambda_function.check_file_lambda.function_name}"
+    principal = "events.amazonaws.com"
+    source_arn = "${aws_cloudwatch_event_rule.check-file-event.arn}"
+}
+
